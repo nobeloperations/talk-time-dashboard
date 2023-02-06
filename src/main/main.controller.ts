bd5de87e927359f1e48b7c8da3dd28766370e785
dan1aa <burmeidan@gmail.com>
2023-02-06 11:33:07 +0200

    main fix

--- src/main/main.controller.ts
+++ src/main/main.controller.ts
@@ -3,26 +3,26 @@ import { MainService } from './main.service';
 import { UrlDto } from 'global.dto';
 import { AddGeneralDto } from 'src/main/dtos/add-general.dto';
 
-@Controller('')
+@Controller('main')
 export class MainController {
 
     constructor(private mainService: MainService){}
 
-    @Get()
+    @Get('')
     @Render('main')
     @HttpCode(200)
     getMain() {
         return this.mainService.getMain()
     }
 
-    @Get('searchlist/:url')
+    @Get('/searchlist/:url')
     @Render('searchlist')
     @HttpCode(200)
     getSearchlist(@Param() params: UrlDto) {
         return this.mainService.getSearchlist(params)
     }
 
-    @Post('addgeneral')
+    @Post('/addgeneral')
     @HttpCode(200)
     addGeneral(@Body() addGeneralBodyDto: AddGeneralDto) {
         return this.mainService.addGeneral(addGeneralBodyDto)
